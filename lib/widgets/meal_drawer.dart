f0e0cafab57f105ee981b6d6a60fe30162f5c6d5
GrafSoul <grafsoul@gmail.com>
2022-08-19 12:02:23 +0500

    feat: Added MealDrawer widget.

---
+++ lib/widgets/meal_drawer.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class MealDrawer extends StatelessWidget {
+  const MealDrawer({Key? key}) : super(key: key);
+
+  Widget buildListTile(String title, IconData icon) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        title,
+        style: const TextStyle(
+          fontFamily: 'RobotoCondensed',
+          fontWeight: FontWeight.bold,
+          fontSize: 24,
+        ),
+      ),
+      onTap: () {
+        //...
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: const EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            color: Theme.of(context).secondaryHeaderColor,
+            child: Text(
+              'Cooking Up!',
+              style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 30,
+                  color: Theme.of(context).primaryColor),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          buildListTile('Meals', Icons.restaurant),
+          buildListTile('Filters', Icons.settings),
+        ],
+      ),
+    );
+  }
+}
